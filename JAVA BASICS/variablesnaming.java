775f3fb8ee7573957d9d553b69cd2faa15217660
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-05 08:13:44 +0530

    java basic

--- JAVA BASICS/variablesnaming.java
+++ JAVA BASICS/variablesnaming.java
@@ -2,7 +2,7 @@ public class variablesnaming {
     public static void main(String[] args) {
         //_ or alaphabet or $
         int uchit=100;
-        int yuvank=99;
+        int yuvank=200;
         System.out.println(uchit+yuvank);
     }
 }
--- variablesnaming.java
+++
@@ -1,8 +0,0 @@
-public class variablesnaming {
-    public static void main(String[] args) {
-        //_ or alaphabet or $
-        int uchit=100;
-        int yuvank=200;
-        System.out.println(uchit+yuvank);
-    }
-}
