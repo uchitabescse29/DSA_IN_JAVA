712cbac4e21b09de280dc484b64ce097a13785a7
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-05 08:38:22 +0530

    input java

--- JAVA_BASICS/input.java
+++ JAVA_BASICS/input.java
@@ -1,5 +1,12 @@
 package JAVA_BASICS;
 
+import java.util.Scanner;
+
 public class input {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r=sc.nextDouble();
+        double a=3.141592*r*r;
+        System.out.println(a);
+    }
 }
