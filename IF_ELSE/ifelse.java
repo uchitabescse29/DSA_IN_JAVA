157a4abbba9bf4dbf7af7759a99aab6032c8f534
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-07 08:53:42 +0530

    if-else program

---
+++ IF_ELSE/ifelse.java
@@ -0,0 +1,15 @@
+package  IF_ELSE;
+ import java.util.Scanner;
+public class ifelse {
+      public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        
+          if(n%2==0){
+            System.out.println("even");
+          }else{
+            System.out.println("odd");
+          }
+      }
+
+}
