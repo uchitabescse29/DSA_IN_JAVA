b8ef33da4db2496ef958ad98708b17390cf00b09
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-19 18:38:38 +0530

    loops6

--- Loops/loops6.java
+++ Loops/loops6.java
@@ -6,6 +6,8 @@ public class loops6 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+
+        //A.P 2,5,8,11,14.....
         for(int i=2;i<3*n-1;i+=3){
             System.out.println(i);
         }
