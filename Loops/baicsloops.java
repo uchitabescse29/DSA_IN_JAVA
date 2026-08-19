debd8fed0aafb00669c7910ecab04b7295bf0f62
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-19 17:51:00 +0530

    basics loop

---
+++ Loops/baicsloops.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class baicsloops {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+          System.out.println("bachoo padhloo yrr");
+        }
+        
+    }
+}
