79e0c6a3a42854a91477f9e2014901f9bfd5ad45
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-19 18:18:51 +0530

    loops4

---
+++ Loops/loops4.java
@@ -0,0 +1,22 @@
+package Loops;
+
+public class loops4 {
+    public static void main(String[] args) {
+        // 154 iterations hai loop ke [170-17+1]
+    //    for (int i = 17; i <=170; i++) {
+    //     if(i%17==0){
+    //         System.out.print(i+" ");
+    //     }
+        
+       
+
+        for (int i = 17; i <=170; i=i+17) {
+           
+
+            
+            System.out.print(i+ " ");
+          
+        }
+    }
+    
+}
