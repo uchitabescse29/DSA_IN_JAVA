ca47b050c30dd0da0c3e9402a0236fce726f4ac2
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-19 18:32:18 +0530

    loops6

---
+++ Loops/loops6.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class loops6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=2;i<3*n-1;i+=3){
+            System.out.println(i);
+        }
+
+
+        
+    }
+    
+}
