46f0cfa436eec64423b48cc6facd39660de004ec
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-19 18:12:33 +0530

    loops3

---
+++ Loops/loops3.java
@@ -0,0 +1,17 @@
+package Loops;
+
+
+
+public class loops3 {
+    public static void main(String[] args) {
+        for (int i=0;i<=100;i++){
+            if(i%2==0){
+               System.out.print(i+" ");
+            }
+          
+            
+        }
+    }
+    }
+    
+
