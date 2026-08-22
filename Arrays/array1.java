43384d0938a390ae0566a236fb96311bcb7f5b7a
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-22 14:56:13 +0530

    array1

---
+++ Arrays/array1.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class array1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                System.out.print(arr[i]+" ");
+            }
+            sc.close();
+        }
+    }
+    
+}
