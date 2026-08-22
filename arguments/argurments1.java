652221a56292cc86eac364bfda212100c07e6783
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-08-22 10:46:10 +0530

    arguments1

---
+++ arguments/argurments1.java
@@ -0,0 +1,11 @@
+package arguments;
+
+public class argurments1 {
+    public static void sum(int a,int b){ //parameter
+       System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        sum(5,8); //argurments
+
+    }
+}
