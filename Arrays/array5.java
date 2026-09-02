d43f10154f31b48f0c27853472999ba73b263e44
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-09-02 21:21:40 +0530

    array5

---
+++ Arrays/array5.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class array5 {
+    public static void main(String[] args) {
+        double[]arr=new double[8];
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+ " ");
+        }
+    }
+}
