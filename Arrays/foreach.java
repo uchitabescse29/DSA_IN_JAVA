d0bf41130b07d20120f7c44ba194b4bc21b0556e
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-09-05 14:59:08 +0530

    for each loop

---
+++ Arrays/foreach.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class foreach {
+    public static void main(String[] args) {
+        int[]arr={5,8,2,6,9};
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
