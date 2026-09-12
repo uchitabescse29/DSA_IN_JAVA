cdcc7220418bc27c2f99cd52beca1aaf50b11b75
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-09-12 11:39:53 +0530

    selection sort

---
+++ Sorting/selectionsort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+public class selectionsort {
+    public static void print(int[]arr){
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr []={5,-2,6,7,2,0,7,2};
+        int n=arr.length;
+        print(arr);
+        for(int i=0;i<n-1;i++){
+            int min=Integer.MAX_VALUE;
+            int mindx=-1;
+            for(int j=i;j<n;j++){
+                if(arr[j]<min){
+                    min=arr[j];
+                    mindx=j;
+
+                }
+
+            }
+            int temp=arr[i];
+            arr[i]=arr[mindx];
+            arr[mindx]=temp;
+               }
+print(arr);
+
+    }
+    
+}
